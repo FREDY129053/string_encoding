b76a3a6542b61e024ea6222b5704f31482050f54
FREDY129053 <fredy1290534518@gmail.com>
2025-02-26 06:28:31 +1000

    ашчЖ ыуй дфиуд штзге

--- src/ProbaInput.tsx
+++ src/ProbaInput.tsx
@@ -70,9 +70,10 @@ export default function ProbabilityInput({
         />
         <label
           htmlFor="floating_text"
+          // className="peer-focus:font-medium absolute text-sm text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 left-0 w-full peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           className="peer-focus:font-medium absolute text-sm text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
-          Enter the length of the sequences(max 5)
+          Enter length of the sequences(max 5)
         </label>
       </div>
 
