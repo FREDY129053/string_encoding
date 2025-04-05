dbd7fed0a90cd25a9c7fae5687da121fed14cb4c
FREDY129053 <fredy1290534518@gmail.com>
2025-04-06 02:32:35 +1000

    fix: base url

---
+++ src/404.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        textAlign: "center",
+        backgroundColor: "#f8f9fa",
+        color: "#343a40",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <h1 style={{ fontSize: "6rem", margin: 0 }}>404</h1>
+      <p style={{ fontSize: "1.5rem", margin: "1rem 0" }}>
+        Oops! The page you're looking for doesn't exist.
+      </p>
+      {/* <Link
+        to="/"
+        style={{
+          marginTop: "1rem",
+          padding: "0.5rem 1rem",
+          fontSize: "1rem",
+          color: "#fff",
+          backgroundColor: "#007bff",
+          textDecoration: "none",
+          borderRadius: "0.25rem",
+        }}
+      >
+        Go Back Home
+      </Link> */}
+    </div>
+  );
+};
+
+export default NotFoundPage;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import FirstHWApp from './Huffman_Fano_HW/App'
-import CiphersHWApp from './Ciphers_HW/App'
+import FirstHWApp from './Huffman_Fano_HW/App';
+import CiphersHWApp from './Ciphers_HW/App';
+import NotFoundPage from './404';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path='/string_encoding' element={<CiphersHWApp />} />
-        {/* <Route path='/ciphers' element={<CiphersHWApp />} /> */}
-        {/* <Route path='/' element={<CiphersHWApp />} /> */}
+        <Route path="/string_encoding" element={<FirstHWApp />} />
+        <Route path="/ciphers" element={<CiphersHWApp />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* Страница 404 */}
       </Routes>
     </Router>
   </StrictMode>,
-)
+);
