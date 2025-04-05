d5a604e090bd07c84c85e7f37296d4196d3f44ac
FREDY129053 <fredy1290534518@gmail.com>
2025-04-06 03:02:33 +1000

    final fix gh pages + router

--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-// import FirstHWApp from './Huffman_Fano_HW/App';
-// import CiphersHWApp from './Ciphers_HW/App';
-// import NotFoundPage from './404';
-import React from 'react';
-
-const FirstHW = () => import('./Huffman_Fano_HW/App');
-const CiphersHW = () => import('./Ciphers_HW/App');
-const NotFound = () => import('./404');
-const FirstHWAppl = React.lazy(FirstHW);
-const CiphersHWAppl = React.lazy(CiphersHW);
-const NotFoundPagel = React.lazy(NotFound);
+import FirstHWApp from './Huffman_Fano_HW/App';
+import CiphersHWApp from './Ciphers_HW/App';
+import NotFoundPage from './404';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/string_encoding" element={<FirstHWAppl />} />
-        <Route path="/ciphers" element={<CiphersHWAppl />} />
-        <Route path="*" element={<NotFoundPagel />} />
-        {/* <Route path="/string_encoding" element={<FirstHWApp />} />
+        <Route path="/string_encoding" element={<FirstHWApp />} />
         <Route path="/ciphers" element={<CiphersHWApp />} />
-        <Route path="*" element={<NotFoundPage />} /> */}
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   </StrictMode>,
